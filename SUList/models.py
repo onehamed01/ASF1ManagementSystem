7842969ba1bbbf2fd22e355562e574143c066cb4
onehamed01 <onehamed01@Gmail.com>
2025-09-29 20:54:34 +0100

    Add models with relations

--- SUList/models.py
+++ SUList/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class ApplicationBase(models.Model):
+    name = models.DateTimeField(auto_now=True)
+    total_app = models.PositiveSmallIntegerField()
+    submitted = models.PositiveSmallIntegerField()
+    amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        abstract = True
+    
+class MonthApplication(ApplicationBase):
+    pass
+
+class DayApplication(ApplicationBase):
+    monthapplication = models.ForeignKey(MonthApplication, on_delete=models.CASCADE)
+
+class ServiceUser(models.Model):
+    serviceuser_name = models.CharField(max_length = 100)
+    references = models.CharField(max_length = 30)
+    status = models.BooleanField(default=False)
+    po_num = models.CharField(max_length = 10)
+    dayapplication = models.ForeignKey(DayApplication, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.references
